6afebaa351303755ca9ebe8ecdf03c7f87ec1087
Iman Ali <iman@example.com>
2026-09-23 20:26:23 +0200

    console application

--- Try3/Try3/Program.cs
+++ Try3/Try3/Program.cs
@@ -70,10 +70,10 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
+  // app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
